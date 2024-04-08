b6b3f431c849de58ba8ba6ec5886118f3a7f459c
Cameron Worthington <cameron.worthington@bbd.co.za>
2024-04-08 18:07:18 +0200

    Oauth added, added all apis except update joke

---
+++ DadJokeWebClientBlazor/DadJokeWebClient/Components/GlobalVariables.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualBasic;
+using System.Net.Http.Headers;
+
+namespace DadJokeWebClient.Components
+{
+    public class GlobalVariables
+    {
+
+        public static string ApiRoot { get; } = "http://ec2-3-250-229-22.eu-west-1.compute.amazonaws.com:5282/api";
+        public static string? AccessToken { get; set; }
+
+        public static Boolean IsAccessTokenValid()
+        {
+            if (AccessToken == null)
+            {
+                return false;
+            }
+            else
+            {
+                var client = new HttpClient();
+                string uri = $"https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={AccessToken}";
+
+                client.BaseAddress = new Uri(uri);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = client.PostAsync(client.BaseAddress, null).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return true;
+
+                }
+                return false;
+            }
+
+        }
+    }
+}
